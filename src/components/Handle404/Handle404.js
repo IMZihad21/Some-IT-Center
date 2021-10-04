6aed3e647260a5d7dfa5d331ba5fab1ea4901d5f
ZèD <imzihad@gmail.com>
2021-10-04 17:37:37 +0600

    Added 404 error handling section
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/components/Handle404/Handle404.js
+++ src/components/Handle404/Handle404.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Handle404 = () => {
     return (
         <div>
-            <h1>404! Page not found.</h1>
+            <h1 className="text-5xl text-red-500 font-bold p-20">404 Error!</h1>
+            <p className="text-xl p-10">The page you are looking for is not available.</p>
         </div>
     );
 };
