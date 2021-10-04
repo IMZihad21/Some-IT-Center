981404f00f59f6500db96cb5f5c5c0bcd800dec6
ZèD <imzihad@gmail.com>
2021-10-04 17:34:02 +0600

    Added support section
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/components/LiveSupport/LiveSupport.js
+++ src/components/LiveSupport/LiveSupport.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const LiveSupport = () => {
     return (
-        <div>
-            <h1>Live Support</h1>
+        <div className="bg-gray-200 rounded-xl shadow-md m-6">
+            <h1 className="text-3xl font-bold py-4">Fill in the info to start a session with a representative</h1>
+            <form className="flex flex-col px-16 py-5 text-center" action="">
+                <input className="rounded py-2 my-1 text-center" placeholder="Your Name" type="text" />
+                <input className="rounded py-2 my-1 text-center" placeholder="Your Email" type="email" />
+                ​<textarea className="rounded text-center" placeholder="Your issue you want to discuss" rows="10" cols="70"></textarea>
+                <button className="rounded-lg bg-red-600 text-white hover:bg-red-500 px-12 py-2 m-2">Submit</button>
+            </form>
         </div>
     );
 };
