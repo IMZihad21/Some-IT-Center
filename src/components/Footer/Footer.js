d6b98a350def94da318d06407bb46d39c833dd1e
ZèD <imzihad@gmail.com>
2021-10-04 17:16:20 +0600

    Complete footer sections
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="bg-gray-200 rounded-xl shadow-inner p-4 mb-4">
-            <h1 className="text-4xl">This is footer</h1>
+        <div className="bg-gray-200 rounded-xl shadow-inner p-4 mb-1">
+            <h1 className="text-xl">Reliable and Easy to access.</h1>
+            <p>Follow us @</p>
+            <i className="fab fa-facebook mx-1"></i>
+            <i className="fab fa-youtube mx-1"></i>
+            <i className="fab fa-twitter mx-1"></i>
+            <i className="fab fa-instagram mx-1"></i>
+            <i className="fab fa-github mx-1"></i>
         </div>
     );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,8 @@ const Header = () => {
     const activeNavStyle = "bg-red-500 hover:bg-red-500 text-white";
     return (
         <div className="bg-gray-200 rounded-xl shadow-md p-4 flex">
-            <h1 className="text-2xl">Some IT Center</h1>
+            <i className="fas fa-building text-gray-600 text-2xl px-3 my-auto"></i>
+            <h1 className="text-2xl font-bold text-gray-600">Some IT Center</h1>
             <div className="ml-auto mr-4 my-auto">
                 <NavLink className={navStyle} activeClassName={activeNavStyle} to="/home">Home</NavLink>
                 <NavLink className={navStyle} activeClassName={activeNavStyle} to="/services">Services</NavLink>
