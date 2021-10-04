045f6b02e653fe42dffe6d9159ccdbd74a8de889
ZèD <imzihad@gmail.com>
2021-10-04 15:04:44 +0600

    Light mode is more clean
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="bg-black text-white p-4">
+        <div className="bg-gray-200 p-4">
             <h1 className="text-4xl">This is footer</h1>
         </div>
     );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,13 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div className="bg-black text-white p-4 flex">
+        <div className="bg-gray-200 p-4 flex">
             <h1 className="text-2xl">Some IT Center</h1>
             <div className="ml-auto mr-4 my-auto">
-                <NavLink className="border rounded-lg bg-gray-800 hover:bg-gray-700 px-4 py-2 mx-2"  to="/">Home</NavLink>
-                <NavLink className="border rounded-lg bg-gray-800 hover:bg-gray-700 px-4 py-2 mx-2"  to="/services">Services</NavLink>
-                <NavLink className="border rounded-lg bg-gray-800 hover:bg-gray-700 px-4 py-2 mx-2"  to="/support">Support</NavLink>
-                <NavLink className="border rounded-lg bg-gray-800 hover:bg-gray-700 px-4 py-2 mx-2"  to="/about">About</NavLink>
+                <NavLink className="border rounded-lg bg-gray-300 hover:bg-gray-100 px-4 py-2 mx-2"  to="/">Home</NavLink>
+                <NavLink className="border rounded-lg bg-gray-300 hover:bg-gray-100 px-4 py-2 mx-2"  to="/services">Services</NavLink>
+                <NavLink className="border rounded-lg bg-gray-300 hover:bg-gray-100 px-4 py-2 mx-2"  to="/support">Support</NavLink>
+                <NavLink className="border rounded-lg bg-gray-300 hover:bg-gray-100 px-4 py-2 mx-2"  to="/about">About</NavLink>
             </div>
         </div>
     );
