463b819b5448b8cffe8f0735b59314798ee97e3e
ZèD <imzihad@gmail.com>
2021-10-04 18:10:11 +0600

    Update animations on NavLinks
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,10 +3,10 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     const navStyle = "rounded-lg bg-gray-300 hover:bg-gray-100 px-4 py-2 mx-2 shadow-md";
-    const activeNavStyle = "bg-red-500 hover:bg-red-500 text-white";
+    const activeNavStyle = "bg-red-500 hover:bg-red-500 text-white animate-pulse";
     return (
         <div className="bg-gray-200 rounded-xl shadow-md p-4 flex">
-            <i className="fas fa-building text-gray-600 text-2xl px-3 my-auto animate-bounce"></i>
+            <i className="fas fa-building text-gray-600 text-2xl px-3 my-auto"></i>
             <h1 className="text-2xl font-bold text-gray-600">Some IT Center</h1>
             <div className="ml-auto mr-4 my-auto">
                 <NavLink className={navStyle} activeClassName={activeNavStyle} to="/home">Home</NavLink>
